8f7ca6f8a0000841e708d58d766d6350fb930762
joaolucasvqf <joaofonseca@alunos.utfpr.edu.br>
2020-01-10 17:57:08 -0300

    add

--- ConstruindoAPI/MinhasTarefasAPI/MinhasTarefasAPI/Controllers/UsuarioController.cs
+++ ConstruindoAPI/MinhasTarefasAPI/MinhasTarefasAPI/Controllers/UsuarioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MinhasTarefasAPI.Models;
 using MinhasTarefasAPI.Repositories.Contracts;
@@ -14,9 +15,11 @@ namespace MinhasTarefasAPI.Controllers
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioRepository _usuarioRepository;
-        public UsuarioController(IUsuarioRepository usuarioRepository)
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public UsuarioController(IUsuarioRepository usuarioRepository, SignInManager<ApplicationUser> signInManager)
         {
             _usuarioRepository = usuarioRepository;
+            _signInManager = signInManager;
         }
         public ActionResult Login([FromBody]UsuarioDTO usuarioDTO)
         {
@@ -26,7 +29,17 @@ namespace MinhasTarefasAPI.Controllers
             {
                 return UnprocessableEntity(ModelState);
             }
+            
             ApplicationUser usuario = _usuarioRepository.Obter(usuarioDTO.Email, usuarioDTO.Senha);
+
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado!");
+            }
+
+            _signInManager.SignInAsync(usuario, false);
+
+            return Ok();
         }
     }
 }
