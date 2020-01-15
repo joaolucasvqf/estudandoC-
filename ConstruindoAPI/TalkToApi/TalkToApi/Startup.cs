e82ac455733b96d1c02783fd38feecbc5d019a82
joaolucasvqf <joaofonseca@alunos.utfpr.edu.br>
2020-01-15 15:40:31 -0300

    Projto pronto para a criação do promeiro Db-Migration

--- ConstruindoAPI/TalkToApi/TalkToApi/Startup.cs
+++ ConstruindoAPI/TalkToApi/TalkToApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TalkToApi.Database;
 
 namespace TalkToApi
 {
@@ -25,6 +27,9 @@ namespace TalkToApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TalkToContext>(cfg => {
+                cfg.UseSqlite("Data Source=Database\\TalkTo.db");
+            });
             services.AddControllers();
         }
 
