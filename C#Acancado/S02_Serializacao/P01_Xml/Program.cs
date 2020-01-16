1efaa12ec5e960a7161daaeee93f1d0141970dcf
joaolucasvqf <joaofonseca@alunos.utfpr.edu.br>
2020-01-16 16:43:28 -0300

    Serialização de XML

---
+++ C#Acancado/S02_Serializacao/P01_Xml/Program.cs
@@ -0,0 +1,22 @@
+﻿using _00_Biblioteca;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace P01_Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "João Lucas", CPF = "123.123.123.11", Email = "joao@joao.com"};
+
+            //XmlSerializer serializer = new XmlSerializer(usuario.GetType());
+            XmlSerializer serializer = new XmlSerializer(typeof(Usuario));
+
+            StreamWriter stream = new StreamWriter(@"C:\Users\User\source\repos\S02_Serializacao\P01_Xml\serializarcaoXML.xml");
+
+            serializer.Serialize(stream , usuario);
+        }
+    }
+}
---
+++ C#Acancado/S02_Serializacao/P02_DeserializarXml/Program.cs
@@ -0,0 +1,23 @@
+﻿using _00_Biblioteca;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace P02_DeserializarXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\User\source\repos\S02_Serializacao\P01_Xml\serializarcaoXML.xml");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Usuario));
+
+            Usuario usuario = (Usuario)serializer.Deserialize(stream);
+
+            Console.WriteLine("Nome" + usuario.Nome + ", CPF: " + usuario.CPF + ", Email: " + usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
