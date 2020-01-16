d2cfe9f40c443e618e27918afddea7ec580925f8
joaolucasvqf <joaofonseca@alunos.utfpr.edu.br>
2020-01-16 17:39:13 -0300

    Serialização, sessão concluída

---
+++ C#Acancado/S02_Serializacao/P03_SerializarJSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using _00_Biblioteca;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace P03_SerializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { Nome = "Maria", CPF = "321.321.321-33", Email = "maria@maria.com" };
+
+            string text = JsonConvert.SerializeObject(usuario);
+
+            StreamWriter stream = new StreamWriter(@"C:\Projetos\Joao\C#Acancado\S02_Serializacao\P03_SerializarJSON\SerializarJSON.json");
+
+            stream.Write(text);
+            stream.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#Acancado/S02_Serializacao/P04_DeserializarJSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using _00_Biblioteca;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace P04_DeserializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Projetos\Joao\C#Acancado\S02_Serializacao\P03_SerializarJSON\SerializarJSON.json");
+
+            Usuario usuario = new Usuario();
+
+            usuario = (Usuario) JsonSerializer.Create().Deserialize(stream, typeof(Usuario));
+
+            Console.WriteLine("Nome" + usuario.Nome + ", CPF: " + usuario.CPF + ", Email: " + usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
